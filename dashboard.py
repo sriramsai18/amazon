420186b77af3c20de92727ecb8aac6ff2c4350ea
SRIRAM SAI <119840345+sriramsai18@users.noreply.github.com>
2025-12-13 22:20:51 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import base64
 
 st.title("Amazon Sales Analytics Dashboard [Power BI + Streamlit]")
 st.write("Built an interactive Power BI dashboard to analyze Amazon sales across regions and categories.When Power BI asked for a Premium subscription to share the live link, I politely refused and outsmarted the problem by recording the dashboard and deploying it as a looping video inside a Streamlit app — because engineers don’t quit, they adapt. 😄Tools: Power BI, DAX, Streamlit, Python")
-video_path = "Amazon.mp4"
+video_path = "Amazondashboard.mp4"
 
 with open(video_path, "rb") as video_file:
     video_bytes = base64.b64encode(video_file.read()).decode()
@@ -16,4 +16,5 @@ video_html = f"""
 """
 
 components.html(video_html, height=800)
+
 
